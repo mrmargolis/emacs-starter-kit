777ce2ad3a6612bc9134f362f9c0a0efbb31e9d8
Matt Margolis <matt.margolis@gettyimages.com>
2010-02-16 18:22:16 -0600

    add a pretty print for XML

--- mrmargolis.el
+++ mrmargolis.el
@@ -6,10 +6,23 @@
 (global-linum-mode 1)
 
 
-
 ;;look in vendor dir and mrmargolis dir for add ons 
 (add-to-list 'load-path (concat dotfiles-dir "/vendor"))
 (add-to-list `load-path (concat dotfiles-dir "/mrmargolis"))
 
 
+(defun bf-pretty-print-xml-region (begin end)
+  "Pretty format XML markup in region. You need to have nxml-mode
+http://www.emacswiki.org/cgi-bin/wiki/NxmlMode installed to do
+this.  The function inserts linebreaks to separate tags that have
+nothing but whitespace between them.  It then indents the markup
+by using nxml's indentation rules."
+  (interactive "r")
+  (save-excursion
+      (nxml-mode)
+      (goto-char begin)
+      (while (search-forward-regexp "\>[ \\t]*\<" nil t) 
+        (backward-char) (insert "\n"))
+      (indent-region begin end))
+    (message "Ah, much better!"))
 
